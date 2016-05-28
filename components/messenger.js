b17cf801b678d3a4493915b62871042e6360aaad
Matthaeus Mueller <matthew@drunkencoder.de>
2016-05-28 18:38:27 +0200

    redirecting each user into an automatically generated room

--- components/messenger.js
+++ components/messenger.js
@@ -1,16 +1,21 @@
 module.exports = {
     init: function (io, winston) {
         io.on('connection', function (socket) {
+            // a client has been connected
             winston.info('User has been connected.');
 
+            // client will be redirected to a dynamic generated room
+            socket.join(socket.id);
+
+
+            // user has logged in
             socket.on('user.init', function (data) {
                 winston.info('User ' + data.user.name + ' has logged in.');
-                //socket.emit('user.join', {hello: 'world'});
             });
 
             socket.on('send.message', function (data) {
-                winston.info(data.message)
-                socket.broadcast.emit('add.message', {message: data.message});
+                winston.info(data.message);
+                socket.broadcast.to(socket.id).emit('add.message', {message: data.message});
             });
 
             // socket.on('user.join', function () {
