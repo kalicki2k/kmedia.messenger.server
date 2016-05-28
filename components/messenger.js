c3093edd07ffa30bb888193fdcaa15b0578bfdc4
Sebastian Kalicki <s.kalicki@bitexpert.de>
2016-05-28 18:41:33 +0200

    Clean up...

--- components/messenger.js
+++ components/messenger.js
@@ -5,29 +5,19 @@ module.exports = {
 
             socket.on('user.init', function (data) {
                 winston.info('User ' + data.user.name + ' has logged in.');
-                //socket.emit('user.join', {hello: 'world'});
             });
 
             socket.on('send.message', function (data) {
-                winston.info(data.message)
-                socket.broadcast.emit('add.message', {message: data.message});
+                winston.info(data.message);
+                socket.broadcast.emit('add.message', {
+                    user: data.user,
+                    message: data.message
+                });
             });
-
-            // socket.on('user.join', function () {
-            //     console.log(user);
-            // });
 
             socket.on('disconnect', function () {
                 winston.info('User disconnected.');
             });
-
-            //
-            // console.log(socket.id)
-            //
-            // socket.emit('news', { hello: 'world' });
-            // socket.on('my other event', function (data) {
-            //     console.log(data);
-            // });
         });
     }
 };
